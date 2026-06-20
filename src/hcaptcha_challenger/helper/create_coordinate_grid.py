43b4194422d348d8028e6aea7721eb9cb21d4b1b
Tat Nguyen Van <nguyenvantat1182002@gmail.com>
2026-06-20 09:24:04 +0700

    fix(core): set Matplotlib backend to Agg to prevent GUI warning

--- src/hcaptcha_challenger/helper/create_coordinate_grid.py
+++ src/hcaptcha_challenger/helper/create_coordinate_grid.py
@@ -2,6 +2,8 @@ from pathlib import Path
 from typing import Union, TypedDict, Tuple, List
 
 import cv2
+import matplotlib
+matplotlib.use('Agg')
 import matplotlib.pyplot as plt
 import numpy as np
 
